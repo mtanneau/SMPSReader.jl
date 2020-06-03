88cd9906718113d5c7fc3163663cadfd738dc232
mtanneau <mathieu.tanneau@gmail.com>
2020-06-03 10:28:26 -0400

    Update tests

--- test/stoch.jl
+++ test/stoch.jl
@@ -7,19 +7,20 @@ function test_indep_discrete()
 
     @test sdat.name == "TEST1"
 
-    @test length(sdat.indeps) == 2
-    @test haskey(sdat.indeps, ("R000001", "X0001"))
-    @test haskey(sdat.indeps, ("R000002", "X0002"))
-
     # check problem data
-    d1 = sdat.indeps["R000001", "X0001"]
-    @test all(d1.vals  .== [6.0, 8.0])
-    @test all(d1.probs .== [0.5, 0.5])
-
-    d2 = sdat.indeps["R000002", "X0002"]
-    @test all(d2.vals  .== [1.0, 2.0, 3.0])
-    @test all(d2.probs .== [0.1, 0.5, 0.4])
-
+    @test length(sdat.indeps) == 2
+    X1 = sdat.indeps[1]
+    X2 = sdat.indeps[2]
+    @test isa(X1, SMPS.ScalarDiscrete)
+    @test X1.row_name == "R000001"
+    @test X1.col_name == "X0001"
+    @test X1.support == [6.0, 8.0]
+    @test X1.p == [0.5, 0.5]
+    @test isa(X2, SMPS.ScalarDiscrete)
+    @test X2.row_name == "R000002"
+    @test X2.col_name == "X0002"
+    @test X2.support == [1.0, 2.0, 3.0]
+    @test X2.p == [0.1, 0.5, 0.4]
 end
 
 function test_blocks_discrete()
@@ -31,38 +32,36 @@ function test_blocks_discrete()
 
     @test sdat.name == "TEST2"
 
-    @test length(sdat.indeps) == 2  # two different blocks
-    @test haskey(sdat.indeps, "BLOCK1")
-    @test haskey(sdat.indeps, "BLOCK2")
+    @test length(sdat.blocks) == 2  # two different blocks
+    b1 = sdat.blocks[1]
+    b2 = sdat.blocks[2]
 
-    # Check that each block was read properly
-    b1 = sdat.indeps["BLOCK1"]
-    @test all(b1.probs .== [0.6, 0.4])
-    @test all(b1.blocks[1] .== [
+    @test length(b1.support) == 2
+    @test b1.p == [0.6, 0.4]
+    @test b1.support[1] == [
         ("C000001", "X0001", 1.1), ("C000002", "X0001", 1.2),
         ("C000001", "X0002", 2.1), ("C000002", "X0002", 2.2),
         ("C000001", "X0003", 3.1), ("C000002", "X0003", 3.2)
-    ])
-    @test all(b1.blocks[2] .== [
+    ]
+    @test b1.support[2] == [
         ("C000001", "X0001", 11.1), ("C000002", "X0001", 11.2),
         ("C000001", "X0002", 12.1), ("C000002", "X0002", 12.2)
-    ])
+    ]
 
-    b2 = sdat.indeps["BLOCK2"]
-    @test all(b2.probs .== [0.25, 0.35, 0.40])
-    @test all(b2.blocks[1] .== [
+    @test length(b2.support) == 3
+    @test b2.p == [0.25, 0.35, 0.4]
+    @test b2.support[1] == [
         ("C000001", "RIGHT", 1.0), ("C000002", "RIGHT", 2.0),
         ("C000003", "RIGHT", 3.0), ("C000004", "RIGHT", 4.0)
-    ])
-    @test all(b2.blocks[2] .== [
+    ]
+    @test b2.support[2] == [
         ("C000001", "RIGHT", 1.1), ("C000002", "RIGHT", 2.1),
         ("C000003", "RIGHT", 3.1)
-    ])
-    @test all(b2.blocks[3] .== [
+    ]
+    @test b2.support[3] == [
         ("C000001", "RIGHT", 1.2), ("C000002", "RIGHT", 2.2),
         ("C000003", "RIGHT", 3.2)
-    ])
-
+    ]
 
 end
 
